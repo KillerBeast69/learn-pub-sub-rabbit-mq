74abb38249e96026fca098c3c841a221eabc969b
KillerBeast69 <omshinde2803@gmail.com>
2026-07-26 15:21:29 +0530

    exchanges and queues

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -21,6 +23,24 @@ func main() {
 	}
 	defer connection.Close()
 	fmt.Println("successfully connected to RabbitMQ server")
+
+	ch, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("failed to open a channel: %v\n", err)
+	}
+	defer ch.Close()
+	fmt.Println("successfully opened a channel")
+
+	state := routing.PlayingState{
+		IsPaused: true,
+	}
+
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, state)
+	if err != nil {
+		log.Fatalf("failed to publish JSON: %v\n", err)
+	}
+
+	// Wait for interrupt signal to gracefully shutdown the server
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
